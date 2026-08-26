7e39369091bbe1874db2c46a5e16694337c510f0
Jack Shepard <jackshepard3183@gmail.com>
2026-08-26 13:34:15 +0200

    Allow optional invoice file when warranty rule permits

--- database/migrations/018_warranty_hardening.sql
+++ database/migrations/018_warranty_hardening.sql
@@ -1,4 +1,5 @@
 ALTER TABLE warranty_registrations
+  MODIFY COLUMN invoice_file VARCHAR(500) NULL,
   ADD COLUMN invoice_required_snapshot TINYINT(1) NULL AFTER registration_days_limit,
   ADD COLUMN fgas_required_snapshot TINYINT(1) NULL AFTER invoice_required_snapshot,
   ADD COLUMN reviewed_at DATETIME NULL AFTER admin_notes;
