478263aaaa480b45919dd84372eff0cebfb53148
Jack Shepard <jackshepard3183@gmail.com>
2026-08-26 15:50:58 +0200

    Add APP_BASE_PATH configuration

--- config/app.php
+++ config/app.php
@@ -6,6 +6,7 @@ return [
     'env' => getenv('APP_ENV') ?: 'production',
     'debug' => filter_var(getenv('APP_DEBUG') ?: false, FILTER_VALIDATE_BOOL),
     'url' => rtrim((string) (getenv('APP_URL') ?: ''), '/'),
+    'base_path' => (string) (getenv('APP_BASE_PATH') ?: ''),
     'key' => (string) (getenv('APP_KEY') ?: ''),
     'timezone' => (string) (getenv('APP_TIMEZONE') ?: 'Europe/Rome'),
     'session_cookie' => (string) (getenv('SESSION_COOKIE') ?: 'idema_session'),
