38c74f4f60d8e9eeb6eb0b3b3f76a3e3cfeca36d
Jack Shepard <jackshepard3183@gmail.com>
2026-08-26 15:25:03 +0200

    Remove redundant warranty certificate index

--- database/migrations/018_warranty_hardening.sql
+++ database/migrations/018_warranty_hardening.sql
@@ -3,6 +3,3 @@ ALTER TABLE warranty_registrations
   ADD COLUMN invoice_required_snapshot TINYINT(1) NULL AFTER registration_days_limit,
   ADD COLUMN fgas_required_snapshot TINYINT(1) NULL AFTER invoice_required_snapshot,
   ADD COLUMN reviewed_at DATETIME NULL AFTER admin_notes;
-
-CREATE INDEX idx_warranty_registrations_certificate
-  ON warranty_registrations (certificate_number);
