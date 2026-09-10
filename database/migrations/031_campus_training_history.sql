8cf784d43365556496623758bf608e2febf92aae
Jack Shepard <jackshepard3183@gmail.com>
2026-09-11 00:20:32 +0200

    Make Campus certificate migration recoverable

--- database/migrations/031_campus_training_history.sql
+++ database/migrations/031_campus_training_history.sql
@@ -1,4 +1,3 @@
 ALTER TABLE event_registrations
-  ADD COLUMN certificate_number VARCHAR(80) NULL AFTER attended,
-  ADD COLUMN certificate_issued_at DATETIME NULL AFTER certificate_number,
-  ADD KEY idx_event_registrations_certificate (certificate_number);
+  ADD COLUMN IF NOT EXISTS certificate_number VARCHAR(80) NULL AFTER attended,
+  ADD COLUMN IF NOT EXISTS certificate_issued_at DATETIME NULL AFTER certificate_number;
