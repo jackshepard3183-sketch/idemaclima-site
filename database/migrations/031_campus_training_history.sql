7666226440481f1dfe0aef57ed75a17cb45f7336
Jack Shepard <jackshepard3183@gmail.com>
2026-09-11 06:14:01 +0200

    Make Campus training migration Aruba compatible

--- database/migrations/031_campus_training_history.sql
+++ database/migrations/031_campus_training_history.sql
@@ -1,3 +1,3 @@
 ALTER TABLE event_registrations
-  ADD COLUMN IF NOT EXISTS certificate_number VARCHAR(80) NULL AFTER attended,
-  ADD COLUMN IF NOT EXISTS certificate_issued_at DATETIME NULL AFTER certificate_number;
+  ADD COLUMN certificate_number VARCHAR(80) NULL AFTER attended,
+  ADD COLUMN certificate_issued_at DATETIME NULL AFTER certificate_number;
