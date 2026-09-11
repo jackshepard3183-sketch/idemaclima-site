0f433f202809a04f3b277cdd795c74b78e674db1
Jack Shepard <jackshepard3183@gmail.com>
2026-09-11 23:29:39 +0200

    Ensure media title cleanup reaches staging

--- database/migrations/034_media_titles.sql
+++ database/migrations/034_media_titles.sql
@@ -1,3 +1,4 @@
+-- Normalize technical mirror prefixes for labels shown in the Media Library.
 UPDATE media_assets
 SET title = TRIM(SUBSTRING(title, 18))
 WHERE title REGEXP '^[A-Fa-f0-9]{16}[[:space:]]';
