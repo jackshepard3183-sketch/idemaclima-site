f04cb23b2fe4857900b4761011e673684c95e7ec
Jack Shepard <jackshepard3183@gmail.com>
2026-09-11 21:57:28 +0200

    Always deploy staging asset migration runner

--- scripts/deploy_changed_ftps.sh
+++ scripts/deploy_changed_ftps.sh
@@ -84,6 +84,7 @@ for required_path in \
   app/Views/public/campus/cat_register.php \
   app/Views/public/campus/event.php \
   app/Views/public/warranty/form_fields.php \
+  scripts/mirror_remote_assets.php \
   app/Core/Seo.php \
   app/Services/CampusMailService.php \
   app/Services/ContactMailService.php \
