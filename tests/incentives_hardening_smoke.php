8e2959e56acacc01633d8bd7ab08d6bdffde6ebc
Jack Shepard <jackshepard3183@gmail.com>
2026-09-13 01:12:22 +0200

    Aggiorna il collaudo delle CTA incentivi

--- tests/incentives_hardening_smoke.php
+++ tests/incentives_hardening_smoke.php
@@ -23,6 +23,8 @@ $checks=[
  ['mail service','incentives_recipients','destinatari configurabili'],['mail service','Reply-To:','risposta al richiedente'],
 ];
 foreach($checks as [$file,$needle,$label])if(!str_contains($files[$file],$needle))throw new RuntimeException('Check Detrazioni fallito: '.$label);
-foreach(['name="message"','type="file"','Schede tecniche dei prodotti'] as $forbidden)if(str_contains($files['EasyTool form'],$forbidden)||str_contains($files['public page'],$forbidden))throw new RuntimeException('Campo o CTA non consentito: '.$forbidden);
+foreach(['name="message"','type="file"'] as $forbidden)if(str_contains($files['EasyTool form'],$forbidden)||str_contains($files['public page'],$forbidden))throw new RuntimeException('Campo non consentito: '.$forbidden);
+if(str_contains($files['EasyTool form'],'Vai alle schede tecniche'))throw new RuntimeException('CTA schede tecniche non consentita nel modulo EasyTool');
+if(substr_count($files['public page'],'Vai alle schede tecniche')!==3)throw new RuntimeException('CTA schede tecniche mancante nelle tre pagine incentivi');
 foreach(['first_name','last_name','region','province','city','postal_code','phone','email','email_confirm','role'] as $field)if(!str_contains($files['EasyTool form'],'name="'.$field.'"'))throw new RuntimeException('Campo EasyTool mancante: '.$field);
 fwrite(STDOUT,"Incentives hardening smoke OK\n");
