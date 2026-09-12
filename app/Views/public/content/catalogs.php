f2055afa18ed6c89d967abcda01e089a9a69fe14
Jack Shepard <jackshepard3183@gmail.com>
2026-09-12 15:16:30 +0200

    Remove duplicated Cataloghi e gamme heading

--- app/Views/public/content/catalogs.php
+++ app/Views/public/content/catalogs.php
@@ -1,6 +1,6 @@
 <?php
 $catalogGroups = [
- 'guide'=>['name'=>'Cataloghi e gamme.','description'=>'Cataloghi generali e linee complete IDEMA®.','items'=>[]],
+ 'guide'=>['name'=>'Cataloghi generali','description'=>'Cataloghi generali e linee complete IDEMA®.','items'=>[]],
  'prodotto'=>['name'=>'Cataloghi di prodotto','description'=>'Documentazione dedicata ai singoli modelli e alle serie principali.','items'=>[]],
  'tecnico'=>['name'=>'Documentazione tecnica','description'=>'Schemi dimensionali e materiale di approfondimento per progettisti e installatori.','items'=>[]],
  'accessori'=>['name'=>'Accessori e sistemi','description'=>'Sistemi di zonificazione, kit e accessori per gli impianti IDEMA®.','items'=>[]],
